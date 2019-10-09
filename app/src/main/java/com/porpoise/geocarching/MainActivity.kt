783385c5997ac6448ed30fe0e830d65e47bf13f0
Silas Sales <ssales@uoguelph.ca>
2019-10-09 19:15:44 -0400

    request location permissions and update some gradle dependencies

--- app/src/main/java/com/porpoise/geocarching/MainActivity.kt
+++ app/src/main/java/com/porpoise/geocarching/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.porpoise.geocarching
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
 import androidx.appcompat.widget.Toolbar
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.navigation.Navigation.findNavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
@@ -14,17 +16,27 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationView
+import android.util.Log
+
 
 class MainActivity : AppCompatActivity(), MapsFragment.OnFragmentInteractionListener {
     override fun onFragmentInteraction(uri: Uri) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
+    // this is just to track which permission was request from onRequestPermissionsResult
+    private val MY_PERMISSIONS_REQUEST_ACCESS_LOCATION = 1
+
     private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /*
+            Let's check for permissions before anything else
+         */
+        checkPermissionsAccess()
 
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
@@ -51,5 +63,35 @@ class MainActivity : AppCompatActivity(), MapsFragment.OnFragmentInteractionList
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    /*
+    Permissions stuff
+     */
+
+    private fun checkPermissionsAccess() {
+        /*
+            If you have any permissions that you need to add, add them here as this will be called
+            whenever we need to confirm we have the correct permissions
+         */
+        // check for fine location permission
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_DENIED) {
+            // In the future we may want to add prompts for the users to know why we need these permissions
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), MY_PERMISSIONS_REQUEST_ACCESS_LOCATION)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when(requestCode) {
+            MY_PERMISSIONS_REQUEST_ACCESS_LOCATION -> {
+                if((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_DENIED)) {
+                    /* welp permission still denined.. we can't do anything without location so we
+                    * need to exit, in the future add a dialog to tell the user this
+                    */
+                    finishAndRemoveTask()
+                }
+                return
+            }
+        }
     }
 }
