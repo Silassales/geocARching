249e3f30dbd2193749283620bb10e5a62f2a1e9b
Silas Sales <ssales@uoguelph.ca>
2019-10-23 17:36:29 -0400

    Feature2

---
+++ app/src/main/java/com/porpoise/geocarching/Util/Constants.kt
@@ -0,0 +1,10 @@
+package com.porpoise.geocarching.Util
+
+object Constants {
+
+    // Guelph
+    const val DEFAULT_LAT = 43.544804
+    const val DEFAULT_LONG = -80.248169
+
+    const val DEFAULT_CACHE_MARKER_SEARCH_RADIUS = 1.0 // km
+}
---
+++ app/src/main/java/com/porpoise/geocarching/firebaseObjects/Cache.kt
@@ -0,0 +1,14 @@
+package com.porpoise.geocarching.firebaseObjects
+
+import com.google.firebase.Timestamp
+import com.google.firebase.firestore.GeoPoint
+import com.google.firebase.firestore.IgnoreExtraProperties
+import com.porpoise.geocarching.Util.Constants.DEFAULT_LAT
+import com.porpoise.geocarching.Util.Constants.DEFAULT_LONG
+
+@IgnoreExtraProperties
+data class Cache(val ar_cord: GeoPoint =  GeoPoint(DEFAULT_LAT, DEFAULT_LONG),
+                 val date_placed: Timestamp = Timestamp.now(),
+                 val description: String = "",
+                 val model: Int = 1,
+                 val name: String = "")
