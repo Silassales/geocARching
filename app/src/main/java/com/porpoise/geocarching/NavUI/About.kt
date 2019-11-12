7aefac1cad93f190252903ecc92f99012822cb5d
Chigozie Onyeije <conyeije@uoguelph.ca>
2019-11-12 16:28:10 -0500

    Update About.kt

--- app/src/main/java/com/porpoise/geocarching/NavUI/About.kt
+++ app/src/main/java/com/porpoise/geocarching/NavUI/About.kt
@@ -8,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.porpoise.geocarching.R
+import com.porpoise.geocarching.MainActivity
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
